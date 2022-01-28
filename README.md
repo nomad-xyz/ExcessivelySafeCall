52b8864d006fa1ef5bcb1a45879604d126aed288
James <james@prestwi.ch>
2022-01-28 13:46:39 -0800

    Ne te verrai-je plu que dans l'éternité?

---
+++ README.md
@@ -0,0 +1,103 @@
+# ExcessivelySafeCall
+
+This solidity library helps you call untrusted contracts safely. Specifically,
+it seeks to prevent _all possible_ ways that the callee can maliciously cause
+the caller to revert. Most of these revert cases are covered by the use of a
+[low-level call](https://solidity-by-example.org/call/). The main difference
+with between `address.call()`call and `address.excessivelySafeCall()` is that
+a regular solidity call will **automatically** copy bytes to memory without
+consideration of gas.
+
+This is to say, a low-level solidity call will copy _any amount of bytes_ to
+local memory. When bytes are copied from returndata to memory, the
+[memory expansion cost
+](https://ethereum.stackexchange.com/questions/92546/what-is-expansion-cost) is
+paid. This means that when using a standard solidity call, the callee can
+**"returnbomb"** the caller, imposing an arbitrary gas cost. Because this gas is
+paid _by the caller_ and _in the caller's context_, it can cause the caller to
+run out of gas and halt execution.
+
+To prevent returnbombing, we provide `excessivelySafeCall` and
+`excessivelySafeStaticCall`. These behave similarly to solidity's low-level
+calls, however, they allow the user to specify a maximum number of bytes to be
+copied to local memory. E.g. a user desiring a single return value should
+specify a `_maxCopy` of 32 bytes. Refusing to copy large blobs to local memory
+effectively prevents the callee from triggering local OOG reversion. We _also_ recommend careful consideration of the gas amount passed to untrusted
+callees.
+
+Consider the following contracts:
+
+```solidity
+contract BadGuy {
+    function youveActivateMyTrapCard() external pure returns (bytes memory) {
+        assembly{
+            revert(0, 1_000_000)
+        }
+    }
+}
+
+contract Mark {
+    function oops(address badGuy) {
+        bool success;
+        bytes memory ret;
+
+        // Mark pays a lot of gas for this copy 😬😬😬
+        (success, ret) == badGuy.call(
+            SOME_GAS,
+            abi.encodeWithSelector(
+                BadGuy.youveActivateMyTrapCard.selector
+            )
+        );
+
+        // Mark may OOG here, preventing local state changes
+        importantCleanup();
+    }
+}
+
+contract ExcessivelySafeSam {
+    using ExcessivelySafeCall for address;
+
+    // Sam is cool and doesn't get returnbombed
+    function sunglassesEmoji(address badGuy) {
+        bool success;
+        bytes memory ret;
+
+        (success, ret) == badGuy.excessivelySafeCall(
+            SOME_GAS,
+            32,  // <-- the magic. Copy no more than 32 bytes to memory
+            abi.encodeWithSelector(
+                BadGuy.youveActivateMyTrapCard.selector
+            )
+        );
+
+        // Sam can afford to clean up after himself.
+        importantCleanup();
+    }
+}
+```
+
+## When would I use this
+
+`ExcessivelySafeCall` prevents malicious callees from affecting post-execution
+cleanup (e.g. state-based replay protection). Given that a dev is unlikely to
+hard-code a call to a malicious contract, we expect most danger to come from
+dynamic dispatch protocols, where neither the callee nor the code being called
+is known to the developer ahead of time.
+
+Dynamic dispatch in solidity is probably _most_ useful for metatransaction
+protocols. This includes gas-abstraction relayers, smart contract wallets,
+bridges, etc.
+
+Nomad uses excessively safe calls for safe processing of cross-domain messages.
+This guarantees that a message recipient cannot interfere with safe operation
+of the cross-domain communication channel and message processing layer.
+
+## Running Tests
+
+- install [Foundry](https://github.com/gakonst/foundry)
+- `forge test`
+
+## A note on licensing:
+
+Tests are licensed GPLv3, as they extend the `DSTest` contract. Non-test work
+is avialable under user's choice of MIT and Apache2.0.
---
+++ src/test/ExcessivelySafeCall.t.sol
@@ -0,0 +1,201 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.10;
+
+import "ds-test/test.sol";
+import "src/ExcessivelySafeCall.sol";
+
+contract ContractTest is DSTest {
+    using ExcessivelySafeCall for address;
+
+    address target;
+    CallTarget t;
+
+    function returnSize() internal pure returns (uint256 _bytes) {
+        assembly {
+            _bytes := returndatasize()
+        }
+    }
+
+    function setUp() public {
+        t = new CallTarget();
+        target = address(t);
+    }
+
+    function testCall() public {
+        bool _success;
+        bytes memory _ret;
+
+        (_success, _ret) = target.excessivelySafeCall(
+            100_000,
+            0,
+            abi.encodeWithSelector(CallTarget.one.selector)
+        );
+        assertTrue(_success);
+        assertEq(_ret.length, 0);
+        assertEq(t.called(), 1);
+
+        (_success, _ret) = target.excessivelySafeCall(
+            100_000,
+            0,
+            abi.encodeWithSelector(CallTarget.two.selector)
+        );
+        assertTrue(_success);
+        assertEq(_ret.length, 0);
+        assertEq(t.called(), 2);
+
+        (_success, _ret) = target.excessivelySafeCall(
+            100_000,
+            0,
+            abi.encodeWithSelector(CallTarget.any.selector, 5)
+        );
+        assertTrue(_success);
+        assertEq(_ret.length, 0);
+        assertEq(t.called(), 5);
+    }
+
+    function testStaticCall() public {
+        bool _success;
+        bytes memory _ret;
+
+        (_success, _ret) = target.excessivelySafeStaticCall(
+            100_000,
+            0,
+            abi.encodeWithSelector(CallTarget.two.selector)
+        );
+        assertEq(t.called(), 0, "t modified state");
+        assertTrue(!_success, "staticcall should error on state modification");
+    }
+
+    function testCopy(uint16 _maxCopy, uint16 _requested) public {
+        uint16 _toCopy = _maxCopy < _requested ? _maxCopy : _requested;
+
+        bool _success;
+        bytes memory _ret;
+
+        (_success, _ret) = target.excessivelySafeCall(
+            100_000,
+            _maxCopy,
+            abi.encodeWithSelector(CallTarget.retBytes.selector, uint256(_requested))
+        );
+        assertTrue(_success);
+        assertEq(_ret.length, _toCopy, "return copied wrong amount");
+
+        (_success, _ret) = target.excessivelySafeCall(
+            100_000,
+            _maxCopy,
+            abi.encodeWithSelector(CallTarget.revBytes.selector, uint256(_requested))
+        );
+        assertTrue(!_success);
+        assertEq(_ret.length, _toCopy, "revert copied wrong amount");
+    }
+
+
+    function testStaticCopy(uint16 _maxCopy, uint16 _requested) public {
+        uint16 _toCopy = _maxCopy < _requested ? _maxCopy : _requested;
+
+        bool _success;
+        bytes memory _ret;
+
+        (_success, _ret) = target.excessivelySafeStaticCall(
+            100_000,
+            _maxCopy,
+            abi.encodeWithSelector(CallTarget.retBytes.selector, uint256(_requested))
+        );
+        assertTrue(_success);
+        assertEq(_ret.length, _toCopy, "return copied wrong amount");
+
+        (_success, _ret) = target.excessivelySafeStaticCall(
+            100_000,
+            _maxCopy,
+            abi.encodeWithSelector(CallTarget.revBytes.selector, uint256(_requested))
+        );
+        assertTrue(!_success);
+        assertEq(_ret.length, _toCopy, "revert copied wrong amount");
+    }
+
+    function testBadBehavior() public {
+        bool _success;
+        bytes memory _ret;
+
+        (_success, _ret) = target.excessivelySafeCall(
+            3_000_000,
+            32,
+            abi.encodeWithSelector(CallTarget.badRet.selector)
+        );
+        assertTrue(_success);
+        assertEq(returnSize(), 1_000_000, "didn't return all");
+        assertEq(_ret.length, 32, "revert didn't truncate");
+
+
+        (_success, _ret) = target.excessivelySafeCall(
+            3_000_000,
+            32,
+            abi.encodeWithSelector(CallTarget.badRev.selector)
+        );
+        assertTrue(!_success);
+        assertEq(returnSize(), 1_000_000, "didn't return all");
+        assertEq(_ret.length, 32, "revert didn't truncate");
+    }
+
+    function testStaticBadBehavior() public {
+        bool _success;
+        bytes memory _ret;
+
+        (_success, _ret) = target.excessivelySafeStaticCall(
+            2_002_000,
+            32,
+            abi.encodeWithSelector(CallTarget.badRet.selector)
+        );
+        assertTrue(_success);
+        assertEq(returnSize(), 1_000_000, "didn't return all");
+        assertEq(_ret.length, 32, "revert didn't truncate");
+
+                (_success, _ret) = target.excessivelySafeStaticCall(
+            2_002_000,
+            32,
+            abi.encodeWithSelector(CallTarget.badRev.selector)
+        );
+        assertTrue(!_success);
+        assertEq(returnSize(), 1_000_000, "didn't return all");
+        assertEq(_ret.length, 32, "revert didn't truncate");
+    }
+
+}
+
+
+contract CallTarget {
+    uint256 public called;
+    constructor () {}
+
+    function one() external {
+        called = 1;
+    }
+
+    function two() external {
+        called = 2;
+    }
+
+    function any(uint256 _num) external {
+        called = _num;
+    }
+
+    function retBytes(uint256 _bytes) public view {
+        assembly {
+            return(0, _bytes)
+        }
+    }
+
+    function revBytes(uint256 _bytes) public view {
+        assembly {
+            revert(0, _bytes)
+        }
+    }
+
+    function badRet() external returns (bytes memory) {
+        retBytes(1_000_000);
+    }
+
+    function badRev() external{
+        revBytes(1_000_000);
+    }
+}
